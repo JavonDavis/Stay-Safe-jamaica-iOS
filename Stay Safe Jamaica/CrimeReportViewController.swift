855a29ac9e24371abe644688ee0f931c63e77ef5
Jamel Reid <jreid@qualityworkscg.com>
2017-07-15 08:47:50 -0500

    created UI for ReportCrimeViewController

---
+++ Stay Safe Jamaica/CrimeReportViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CrimeReportViewController.swift
+//  Stay Safe Jamaica
+//
+//  Created by Jamel Reid  on 7/15/17.
+//  Copyright © 2017 Stay Safe Jamaica. All rights reserved.
+//
+
+import UIKit
+
+class CrimeReportViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
