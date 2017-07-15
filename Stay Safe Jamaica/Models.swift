8a9ab429da4c42c25430130a8e2dbd8b853e74ff
Jase-Omeileo West <omeileo@icloud.com>
2017-07-15 16:36:30 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Stay Safe Jamaica.xcodeproj/project.pbxproj

---
+++ Stay Safe Jamaica/Models.swift
@@ -0,0 +1,63 @@
+//
+//  Models.swift
+//  Stay Safe Jamaica
+//
+//  Created by Jamel Reid  on 7/15/17.
+//  Copyright © 2017 Stay Safe Jamaica. All rights reserved.
+//
+
+import Foundation
+
+
+struct Report {
+    
+    var category: String?
+    var description: String?
+    var lat: Double?
+    var long: Double?
+    
+    init (category: String?, description: String?, lat: Double?, long: Double?) {
+        
+        self.category = category
+        self.description = description
+        self.lat = lat
+        self.long = long
+    }
+    
+    static func buildObject(_ report: Report?) -> [String:Any] {
+        
+        let reportObject = [
+            "category": report?.category ?? "",
+            "description": report?.description ?? "",
+            "location" : [
+                "lat": report?.lat,
+                "long": report?.long
+            ] as! [String: Double]
+        
+        ] as [String : Any]
+        
+        return reportObject
+    }
+    
+    static func buildReports(_ reportObjects: [String: Any]) -> [Report] {
+        
+        var reportArray = [Report]()
+        
+        for (_, value) in reportObjects {
+            
+            if let object = value as? [String: Any] {
+                if let location = object["location"] as? [String:Double] {
+        
+                    let reportObject = Report(category: object["category"] as? String, description: object["description"] as? String, lat: location["lat"]!, long: location["long"]! )
+                    
+                    reportArray.append(reportObject)
+                }
+            }
+        }
+        
+        print(reportArray)
+        
+        return reportArray
+        
+    }
+}
